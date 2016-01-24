b6394c9e7d840dd6ac21e40f717bfd9ad8ebeb14
Steve Richardson <steve@makeit-secure>
2016-01-24 18:15:48 -0500

    change to use config file

--- databases/update_listener.py
+++ databases/update_listener.py
@@ -1,6 +1,17 @@
+import ConfigParser
 from flask import Flask
 import httplib2
 import hashlib
+
+Config = ConfigParser.ConfigParser()
+Config.read('updater.ini')
+DEBUG = Config.getbool('General', 'Debug')
+OutputFile = Config.get('General', 'OutputFile')
+FlaskHost = Config.get('Flask', 'Host')
+FlaskPort = Config.getint('Flask', 'Port')
+AuthUsername = Config.get('AuthBackend', 'AuthUsername')
+AuthPassword = Config.get('AuthBackend', 'AuthPassword')
+UpdateURL = Config.get('AuthBackend', 'UpdateURL')
 
 app = Flask(__name__)
 
@@ -11,24 +22,27 @@ def hello():
 @app.route('/refresh')
 def refresh():
     h = httplib2.Http(".cache")
-    #h.add_credentials('name', 'password')
-    resp, content = h.request("http://192.168.0.1/",
+    h.add_credentials(AuthUsername, AuthPassword)
+    resp, content = h.request(UpdateURL,
                               headers={'cache-control':'no-cache'})
 
-    print resp
+    if DEBUG:
+        print resp
     
-    if resp['status'] == '200' and resp['content-type'] == 'text/html':
-        try:
-            with open('update.acl', 'w') as f:
-                f.write(content)
+    if resp['status'] == '200':
+        if resp['content-type'] == 'text/plain':
+            try:
+                with open(OutputFile, 'w') as f:
+                    f.write(content)
             
-            return 'Success ' + hashlib.md5(content).hexdigest()
-        except:
-            return 'Error could not write output file'
-        
+                    return 'Success: ' + hashlib.md5(content).hexdigest()
+            except:
+                return 'Error: could not write output file'
+        else:
+            return 'Error: content type not text/plain on response'
     else:
-        return 'Error ' + resp['status']
+        return 'Error: received ' + resp['status'] + ' on response'
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host=FlaskHost, port=FlaskPort)
